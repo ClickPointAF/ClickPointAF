5b1d65017582d0aec8b16489671d9bad551f72e7
Jorge Chavez <chavej28@nala.roche.com>
2024-03-30 09:06:33 -0600

    First files

---
+++ WebDriverProvider/Classes/DriverMethods.cs
@@ -0,0 +1,76 @@
+﻿//using OpenQA.Selenium;
+//using OpenQA.Selenium.Edge;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+//using WebDriverProvider.Enums;
+//using WebDriverProvider.Interfaces;
+//using WebDriverManager;
+//using WebDriverManager.DriverConfigs.Impl;
+//using OpenQA.Selenium.Remote;
+
+//namespace WebDriverProvider.Classes;
+//public partial class WebDriverFactory
+//{
+//    private void SetupTimeouts()
+//    {
+//        _webDriver.Manage().Timeouts().PageLoad = _configuration.PageLoad;
+//        _webDriver.Manage().Timeouts().ImplicitWait = _configuration.ImplicitWait;
+//    }
+
+//    public IWebDriver GetInstanceOf()
+//    {
+//        return _webDriver;
+//    }
+//    public void NavigateToBaseUrl()
+//    {
+//        _webDriver.Navigate().GoToUrl(_configuration.BaseUrl);
+//    }
+
+//    public void NavigateToUrl(string url)
+//    {
+//        if (string.IsNullOrWhiteSpace(url))
+//            throw new ArgumentException($"'{nameof(url)}' cannot be null or whitespace.", nameof(url));
+
+//        _webDriver.Navigate().GoToUrl(url);
+//    }
+
+//    public string GetCurrentUrl()
+//    {
+//        var url = _webDriver.Url;
+
+//        return url;
+//    }
+
+//    public string GetCurrentTitle()
+//    {
+//        var title = _webDriver.Title;
+
+//        return title;
+//    }
+
+//    public void SwitchToFirstTab()
+//    {
+//        var firstTab = _webDriver.WindowHandles.First();
+
+//        _webDriver.SwitchTo().Window(firstTab);
+//    }
+
+//    public void SwitchToLastTab()
+//    {
+//        var lastTab = _webDriver.WindowHandles.Last();
+
+//        _webDriver.SwitchTo().Window(lastTab);
+//    }
+
+//    public void CloseTab()
+//    {
+//        _webDriver.Close();
+//    }
+
+//    public void TerminateWebDriver()
+//    {
+//        _webDriver.Close();
+//        _webDriver.Quit();
+//        _webDriver.Dispose();
+//    }
+//}
---
+++ WebDriverProvider/Classes/WebDriverFactory.cs
@@ -0,0 +1,75 @@
+﻿//using OpenQA.Selenium;
+//using OpenQA.Selenium.Edge;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+//using WebDriverProvider.Enums;
+//using WebDriverProvider.Interfaces;
+//using WebDriverManager;
+//using WebDriverManager.DriverConfigs.Impl;
+//using OpenQA.Selenium.Remote;
+//using WebDriverProvider.Configurations;
+
+//namespace WebDriverProvider.Classes;
+//public partial class WebDriverFactory
+//{
+//    private readonly IWebDriverConfiguration _configuration;
+//    private readonly IWebDriver _webDriver;
+
+//    public IWebDriverConfiguration Configuration => _configuration;
+
+//    public WebDriverFactory(IWebDriverConfiguration configuration)
+//    {
+//        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+//        _webDriver = InstantiateWebDriver();
+//        InitializeWebDriver(_webDriver);
+//    }
+
+//    private IWebDriver InstantiateWebDriver()
+//    {
+//        IBrowserOptionsConfigurator optionsConfigurator = _configuration.Browser switch
+//        {
+//            Browser.Chrome => new ChromeOptionsConfigurator(),
+//            Browser.Firefox => new FirefoxOptionsConfigurator(),
+//            Browser.Edge => new EdgeOptionsConfigurator(),
+//            _ => throw new NotSupportedException($"The browser {_configuration.Browser} is not supported."),
+//        };
+
+//        var options = optionsConfigurator.ConfigureOptions(_configuration.IsHeadless);
+//        if(_configuration.IsRemote)
+//        {
+//            return InstantiateRemoteWebDriver(options, _configuration.SeleniumGridUrl);
+//        }
+//        return InstantiateLocalWebDriver(options);
+//    }
+
+//    private IWebDriver InstantiateRemoteWebDriver(DriverOptions options, string seleniumGridUrl)
+//    {
+//        var uri = new Uri(seleniumGridUrl);
+//        return new RemoteWebDriver(uri, options.ToCapabilities());
+//    }
+
+//    private IWebDriver InstantiateLocalWebDriver(DriverOptions options)
+//    {
+//        switch (options)
+//        {
+//            case ChromeOptions chromeOptions:
+//                new DriverManager().SetUpDriver(new ChromeConfig());
+//                return new ChromeDriver(chromeOptions);
+//            case FirefoxOptions firefoxOptions:
+//                new DriverManager().SetUpDriver(new FirefoxConfig());
+//                return new FirefoxDriver(firefoxOptions);
+//            case EdgeOptions edgeOptions:
+//                new DriverManager().SetUpDriver(new EdgeConfig());
+//                return new EdgeDriver(edgeOptions);
+//            default:
+//                throw new NotSupportedException("Unsupported browser options type.");
+//        }
+//    }
+
+//    private void InitializeWebDriver(IWebDriver webDriver)
+//    {
+//        webDriver.Manage().Window.Maximize();
+//        webDriver.Manage().Cookies.DeleteAllCookies();
+//        SetupTimeouts();
+//    }
+//}
